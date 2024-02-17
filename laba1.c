5caf7df90853d635c145a8b5bde8c543b9b30c9a
ree-zanus <39688921+ree-zanus@users.noreply.github.com>
2024-02-17 14:06:34 +1000

    Update laba1.c

--- laba1.c
+++ laba1.c
@@ -6,28 +6,34 @@ int main()
     float a,b,c;
     float D, x1,x2;
     int x;
-    do {
-    printf("Введите коэффициент а:");
-    scanf("%f",&a);
-    printf("Введите коэффициент b:");
-    scanf("%f",&b);
-    printf("Введите коэффициент c:");
-    scanf("%f",&c);
+    printf("Для старта нажмите 1");
+    scanf("%d/n", &x);
+    while (x==1) {
+        printf("Введите коэффициент а:");
+        scanf("%f",&a);
+        printf("Введите коэффициент b:");
+        scanf("%f",&b);
+        printf("Введите коэффициент c:");
+        scanf("%f",&c);
     
-    D=b*b-4*a*c;
+        D=b*b-4*a*c;
     
-    if (D>0) {
-       x1=(-b+sqrt(D))/(2*a);
-       x2=(-b-sqrt(D))/(2*a);
-       printf("Корни уравнения x1=%f, x2=%f\n", x1, x2);
-    } else if (D==0) { 
-        x1= -b/(2*a);
-        printf("Уравнение имеет один корень x=%f\n", x1);
-    } else if (D<0) {
+        if (D>0) 
+        {
+            x1=(-b+sqrt(D))/(2*a);
+            x2=(-b-sqrt(D))/(2*a);
+            printf("Корни уравнения x1=%f, x2=%f\n", x1, x2);
+        } 
+        else if (D==0)
+        { 
+            x1= -b/(2*a);
+            printf("Уравнение имеет один корень x=%f\n", x1);
+        } 
+        else if (D<0) 
+        {
         printf(" Уравнение не имеет корней\n");
+        }
+     
     }
-        printf("Для повторения нажмите 1");
-        scanf("%d", &x);
-    } while(x==1);
-return 0;
+    return 0;
 }
