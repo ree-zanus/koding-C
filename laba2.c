d21918869799c272006e032ee6e3fd5493ab4dcb
ree-zanus <39688921+ree-zanus@users.noreply.github.com>
2024-02-17 14:30:26 +1000

    Update laba2.c

--- laba2.c
+++ laba2.c
@@ -7,23 +7,28 @@
 /* 3. вывести на консоль квадрат  данной матрицы.*/
 #include <stdio.h>
 int main()
+#include <stdio.h>
+int main()
 {
     double arr[3][3];
     double sum_dg = 0;
     double sum_dp = 0;
     printf("Введите значения массива:");
     
-     for(int i = 0; i<3; i++) {
-        for(int j = 0; j<3; j++) {
+    for(int i = 0; i<3; i++) 
+    {
+        for(int j = 0; j<3; j++) 
+        {
             scanf("%lf", &arr[i][j]); 
         }
     }    
-     for (int i = 0; i < 3; i++) {
+        for (int i = 0; i < 3; i++) 
+    {
         sum_dg += arr[i][i];
         sum_dp += arr[i][2-i]; 
     } 
-    printf("Сумма по главной диагонали: %lf\n", sum_dg);
-    printf("Сумма по побочной диагонали: %lf\n", sum_dp);
+        printf("Сумма по главной диагонали: %lf\n", sum_dg);
+        printf("Сумма по побочной диагонали: %lf\n", sum_dp);
      
     return 0;
 }
