f1ceaa04ab9f0851b92097332db2b3826b6cf970
ree-zanus <39688921+ree-zanus@users.noreply.github.com>
2024-03-09 10:41:18 +1000

    Update laba2.c

--- laba2.c
+++ laba2.c
@@ -51,20 +51,28 @@ int main()
 
 int main() {
     int arr[2][2];
+    int kv[2][2];
     
+ 
     printf("Введите значения массива:\n");
+    for (int i = 0; i < 2; i++) {
+        for (int j = 0; j < 2; j++) {
+            scanf("%d", &arr[i][j]);
+        }
+    }
+    
     
     for (int i = 0; i < 2; i++) {
         for (int j = 0; j < 2; j++) {
-            scanf("%d", &arr[i][j]); 
+            kv[i][j] = arr[i][0] * arr[0][j] + arr[i][1] * arr[1][j];
         }
-    }    
+    }
+    
     
     printf("Квадрат матрицы:\n");
     for (int i = 0; i < 2; i++) {
         for (int j = 0; j < 2; j++) {
-            int Kv = arr[i][j] * arr[i][j];
-            printf("%d ", Kv);
+            printf("%d ", kv[i][j]);
         }
         printf("\n");
     }
