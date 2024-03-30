36dd0a6ca1654ba8157f2584dca79c2520ece4eb
ree-zanus <39688921+ree-zanus@users.noreply.github.com>
2024-03-30 14:47:11 +1000

    Update laba4.c

--- laba4.c
+++ laba4.c
@@ -1,1 +1,50 @@
+#include <stdio.h>
+#include "tr.h"
 
+int main(){
+    int a,b,c,p;
+    double s;
+    printf("Введите a,b,c ");
+    scanf("%d %d %d", &a, &b, &c);
+    
+    if (valid(a,b,c)){
+        p = perim(a,b,c);
+        printf("P=%d\n", p);
+        s = area(a,b,c);
+        printf("S=%lf", s);
+        return 0;
+    } else {
+        printf("не существует");
+        
+    }
+    
+    return 0;
+}
+
+#include <math.h>
+
+int valid(int side1, int side2, int side3){
+    if ((side1<side2+side3) & (side2<side1+side3) & (side3<side1+side2)){
+        return 1;
+    } else {
+        return 0;
+    }
+}
+    
+
+int perim(int side1, int side2, int side3){
+    int per;
+    per=side1+side2+side3;
+    return per;
+}
+
+double area(int side1, int side2, int side3){
+    double ar;
+    int per_2;
+    per_2= (side1+side2+side3)/2;
+    ar=sqr(per_2*(per_2-side1)*(per_2-side2)*(per_2-side3));
+    return ar;
+}
+
+int perim(int side1, int side2, int side3);
+double area(int side1, int side2, int side3);
