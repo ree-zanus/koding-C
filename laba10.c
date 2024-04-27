273df26ecfb7a1192e0927354f9ee12538215fd7
ree-zanus <39688921+ree-zanus@users.noreply.github.com>
2024-04-27 15:11:56 +1000

    Create laba10.c

---
+++ laba10.c
@@ -0,0 +1,34 @@
+
+#include <stdio.h>
+#define Max(a, b) (a) >= (b) ? (a) : (b)
+
+int main()
+{
+    int num[100];
+    int count = 0;
+    
+    printf("Введите числа:");
+
+    while (scanf("%d", &num[count])==1){
+        
+  //      printf("otvet %d", num[count]);
+        
+        if (num[count]==1) break;
+        
+        count++;
+    }
+    printf("Massive is: ");
+    for (int i=0; i<=count;i++){
+        
+        printf(" %d ", num[i]);
+    }
+    
+    int max_num = num[0];
+    for (int i=1; i<count;i++){
+        
+        max_num= Max(max_num, num[i]);
+    }
+    printf("Максимальное числo: %d\n",max_num);
+    return 0;
+}
+
