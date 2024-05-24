c4bb588bf8f4e0932cd3d80908e1046302d31532
ree-zanus <39688921+ree-zanus@users.noreply.github.com>
2024-05-24 23:23:38 +1000

    Update laba10.c

--- laba10.c
+++ laba10.c
@@ -1,19 +1,19 @@
- #include <stdio.h>
-#define Max(a, b) ((a) >= (b) ? (a) : (b))
+#include <stdio.h>
+#define Max(a, b) ((a) >= (b) ? (a) : (b)) //Затем определяется макрос Max(a, b), который возвращает максимальное значение из двух переданных ему аргументов a и b.
 
 int main() {
     int n;
     printf("Введите количество чисел: ");
     scanf("%d", &n);
 
-    int numbers[n];
+    int numbers[n]; // Создается массив numbers с размером n для хранения введенных чисел. 
     printf("Введите числа: ");
     for (int i = 0; i < n; i++) {
         scanf("%d", &numbers[i]);
     }
 
-    int max = numbers[0];
-    double sum = 0;
+    int max = numbers[0]; //сохранение макисмального числа
+    double sum = 0; //для сохранения суммы чисел
     for (int i = 0; i < n; i++) {
         max = Max(max, numbers[i]);
         sum += numbers[i];
