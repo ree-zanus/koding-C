2493289bd054c741ec91c1fb9205099e41d5fe24
ree-zanus <39688921+ree-zanus@users.noreply.github.com>
2024-05-24 23:19:39 +1000

    Update laba10.c

--- laba10.c
+++ laba10.c
@@ -1,34 +1,28 @@
-   
-#include <stdio.h>
-#define Max(a, b) (a) >= (b) ? (a) : (b)
- 
-int main()
-{
-    int num[100];
-    int count = 0;
-    
-    printf("Введите числа:");
+ #include <stdio.h>
+#define Max(a, b) ((a) >= (b) ? (a) : (b))
 
-    while (scanf("%d", &num[count])==1){
-        
-  //      printf("otvet %d", num[count]);
-        
-        if (num[count]==1) break;
-        
-        count++;
+int main() {
+    int n;
+    printf("Введите количество чисел: ");
+    scanf("%d", &n);
+
+    int numbers[n];
+    printf("Введите числа: ");
+    for (int i = 0; i < n; i++) {
+        scanf("%d", &numbers[i]);
     }
-    printf("Massive is: ");
-    for (int i=0; i<=count;i++){
-        
-        printf(" %d ", num[i]);
+
+    int max = numbers[0];
+    double sum = 0;
+    for (int i = 0; i < n; i++) {
+        max = Max(max, numbers[i]);
+        sum += numbers[i];
     }
-    
-    int max_num = num[0];
-    for (int i=1; i<count;i++){
-        
-        max_num= Max(max_num, num[i]);
-    }
-    printf("Максимальное числo: %d\n",max_num);
+
+    int arif = sum / n;
+
+    printf("Максимальное число: %d\n", max);
+    printf("Среднее арифметическое: %d\n", arif);
+
     return 0;
 }
-
